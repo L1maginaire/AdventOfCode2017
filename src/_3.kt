76ebc53f49a5520328496a4372f9b9804d4401bd
l1maginaire <l1maginaire00@gmail.com>
2019-01-21 23:43:56 +0200

    3

--- src/_3.kt
+++ src/_3.kt
@@ -1,116 +1,110 @@
 private const val X = 347991
- var cc = 0
 
 fun main(args: Array<String>) {
-//    var x = 347991.toDouble()
-    /*for (i in 1..591 step 2) {
-        printlnln("$i: ${i.toDouble().pow(2)}")
-    }*/
 /*
-    println(rowCounter(8))
-    println(rowCounter(24))
-    println(rowCounter(48))
-    println(getNumArrayForRow(7))
-    println(getNumArrayForRow(5))
-    println(getNumArrayForRow(3))
-    println(getNumArrayForRow(11))
-
-    println(findFirstCenter(7, 3))
-    println(findFirstCenter(5, 2))
-    println(findFirstCenter(3, 1))
-    println(findFirstCenter(13, 6))
+    println(getRowNumberFromSouthEastDiagonalesNumber(121.toFloat()))
+    println(getRowNumberFromSouthEastDiagonalesNumber(81.toFloat()))
+    println(getRowNumberFromSouthEastDiagonalesNumber(49.toFloat()))
+    println(getRowNumberFromSouthEastDiagonalesNumber(25.toFloat()))
     println()
-    println(returnPos(3, 47, 49, 46, false))
-    println(returnPos(3, 48, 49, 46, false))
-    println(returnPos(3, 45, 46, 43, true))
-    println(returnPos(3, 44, 46, 43, true))
-    println()*/
-//    f(7,3, 36)
-//    f(5, 2, 10)
-//    f(7, 3, 29)
-//    f(3, 1, 9)
-/*
-    f(3,1, 2)
-    f(3,1, 3)
-    f(3,1, 4)
-    f(3,1, 5)
-    f(3,1, 6)
-    f(3,1, 7)
-    f(3,1, 8)
-    f(3,1, 9)
-*/
+    println(getArrayCompleteCircleOfRow(3))
+    println(getArrayCompleteCircleOfRow(5))
+    println(getArrayCompleteCircleOfRow(7))
+    println(getArrayCompleteCircleOfRow(9))
+    println()
+    println(getOtherPartOfDistance(49, 3, false, 46))
+    println(getOtherPartOfDistance(49, 3, false, 47))
+    println(getOtherPartOfDistance(49, 3, false, 48))
+    println(getOtherPartOfDistance(49, 3, false, 49))
+    println()
+    println(getOtherPartOfDistance(46, 3, true, 46))
+    println(getOtherPartOfDistance(46, 3, true, 45))
+    println(getOtherPartOfDistance(46, 3, true, 44))
+    println(getOtherPartOfDistance(46, 3, true, 43))
+    println()
+    println(findFourCenters(getArrayCompleteCircleOfRow(3),1).toSortedSet())
+    println(findFourCenters(getArrayCompleteCircleOfRow(5),2).toSortedSet())
+    println(findFourCenters(getArrayCompleteCircleOfRow(7),3).toSortedSet())
+
+    for (i in 26..49) {
+        println("X: $i")
+        println(f(3, i))
+    }
     for (i in 10..25) {
-        f2(5, 2, i)
-    }
-    println("XX $cc")
-//    f(5,2, 23)
+        println("X: $i")
+        println(f(2, i))
+    }*/
+
+    println(getMangattanDistance(X))
 }
 
-fun rowCounter(number: Int): Pair<Int, Int> {
-    var prev = 0
-    for ((row, i) in (1..500 step 2).withIndex()) {
-        if (i * i >= number) {
-            return Pair(prev, row - 1)
-        }
-        prev = i * i
+fun getNighToXSouthEastDiagonalesNumber(x: Int): Int {
+    var previous = 1
+    while (x > previous * previous) {
+        previous += 2
     }
-    return Pair(0, 0)
+    return previous
 }
 
-fun getNumArrayForRow(i: Int): List<Int> {
-    val round = i + i + (i - 2) + (i - 2)
-    var countdown2 = i * i
+fun getRowNumberFromSouthEastDiagonalesNumber(number: Float) = ((number - 1) / 2).toInt()
+
+fun getArrayCompleteCircleOfRow(diagonaleNumberRooted: Int): List<Int> {
+    val round = 2 * diagonaleNumberRooted + 2 * (diagonaleNumberRooted - 2)
+    var max = diagonaleNumberRooted * diagonaleNumberRooted
     val array = mutableListOf<Int>()
     for (j in 1..round) {
-        array.add(countdown2--)
+        array.add(max--)
     }
     return array
 }
 
-fun findFirstCenter(amountInRow: Int, row: Int) = amountInRow * amountInRow - row
-
-fun findNextCenter(amountInRow: Int, row: Int, startsFrom: Int) = startsFrom - amountInRow
-
-fun returnPos(row: Int, x: Int, end: Int, begin: Int, viceVersa: Boolean): Int {
-    cc++
-    if (x == end) {
-        println("X1")
-        return 2 * row
-    }
-    if (x == begin) {
-        println("x2")
-        return 0
-    }
+fun getOtherPartOfDependency(max: Int, rowNumber: Int, viceVersa: Boolean, x: Int, isFirst: Boolean = false): Int {
     if (!viceVersa) {
-        for ((counter, i) in (begin..end).withIndex()) {
-            if (x == i) {
-                println("X3")
-                return counter
-            }
+        val min = max
+        val max = min + rowNumber
+        for ((index, i) in (min..max).withIndex()) {
+            if (x == i) return index
         }
     } else {
-        for ((counter, i) in (end downTo begin).withIndex()) {
+        val min = if (isFirst) max - rowNumber + 1 else max - rowNumber
+        var reverseCounter = if (isFirst) rowNumber - 1 else rowNumber
+        for (i in min..max) {
             if (x == i) {
-                println("X4")
-                return counter
+                return reverseCounter
+            } else {
+                reverseCounter--
             }
+        }
+    }
+    return -1
+}
+
+fun findFourCenters(circleArray: List<Int>, row: Int): IntArray {
+    val fourCenters = IntArray(4)
+    var x = circleArray.first() - row
+    for (i in 0..3) {
+        fourCenters[i] = x
+        x -= row * 2
+    }
+    return fourCenters
+}
+
+fun getMangattanDistance(x: Int): Int {
+    val diagonaleNumberRooted = getNighToXSouthEastDiagonalesNumber(X)
+    val row = getRowNumberFromSouthEastDiagonalesNumber(diagonaleNumberRooted.toFloat())
+    val set = findFourCenters(getArrayCompleteCircleOfRow(diagonaleNumberRooted), row).toSortedSet()
+    set.forEachIndexed { index, i ->
+        if (index == 0) {
+            val a = getOtherPartOfDependency(i, row, true, x, true)
+            if (a != -1) return a + row
+            val a2 = getOtherPartOfDependency(i, row, false, x, true)
+            if (a2 != -1) return a2 + row
+        } else {
+            val a = getOtherPartOfDependency(i, row, true, x)
+            if (a != -1) return a + row
+            val a2 = getOtherPartOfDependency(i, row, false, x)
+            if (a2 != -1) return a2 + row
         }
     }
     return 0
-}
-
-fun f2(sqr: Int, row: Int, x: Int) {
-    println("actual x: $x")
-    val array = getNumArrayForRow(sqr)
-    println(array)
-    var viceVersa = false
-    for (i in array.first() downTo array.last() step row) {
-        println("XXX $i")
-        val gain = returnPos(row, x, i, i - row, viceVersa)
-        if (gain > 0) {
-            println(gain + row)
-            break
-        }
-        viceVersa = !viceVersa
-    }
 }
